678782294934f6ec732093a0d55e7bc4d9ae7888
Allen-kevin <wanwenkai29@gmail.com>
2021-02-07 16:28:24 +0800

    Added ops of power about 2

---
+++ C-Library/include/tools.h
@@ -0,0 +1,19 @@
+/* This file used to record some functions which are classcial. */
+
+static inline bool is_power_of_2(uint32_t n)
+{
+	/* eg. (1000 & 0111) == 0 */
+	return (n != 0 && ((n & (n -1)) == 0));
+}
+
+static inline uint32_t roundup_power_of_2(uint32_t a)
+{
+	if (a == 0)
+		return 0;
+
+	uint32_t position = 0;
+	for (int i = a; i != 0; i >>= 1)
+		position++;
+
+	return static_cast<uint32_t>(1 << position);
+}
