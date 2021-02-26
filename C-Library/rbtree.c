6d0c6394bcb1cf5fc1fa4647cc64a4f8b8072452
Allen-kevin <wanwenkai29@gmail.com>
2021-02-26 09:23:58 +0800

    red-black tree

--- C-Library/rbtree.c
+++ C-Library/rbtree.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include "rbtree.h"
 
-#define rb_parent(r)	((r)->parent)
-#define rb_color(r)		((r)->color)
-#define rb_is_red(r)	((r)->color==RED)
-#define rb_is_black(r)	((r)->color==BLACK)
-#define rb_set_red(r)	do {(r)->color = RED;} while (0)
-#define rb_set_black(r)	do {(r)->color = BLACK;} while (0)
-#define rb_set_parent(r, p) do {(r)->parent = (p);} while (0)
-#define rb_set_color(r, c) do {(r)->color = (c);} while (0)
+#define RB_PARENT(r)	((r)->parent)
+#define RB_COLOR(r)		((r)->color)
+#define RB_IS_RED(r)	((r)->color==RED)
+#define RB_IS_BLACK(r)	((r)->color==BLACK)
+#define RB_SET_RED(r)	do {(r)->color = RED;} while (0)
+#define RB_SET_BLACK(r)	do {(r)->color = BLACK;} while (0)
+#define RB_SET_PARENT(r, p) do {(r)->parent = (p);} while (0)
+#define RB_SET_COLOR(r, c) do {(r)->color = (c);} while (0)
 
 /* Create a red-black tree */
 RBROOT* create_rbtree()
@@ -63,7 +63,7 @@ static RBTREE* _iterative_rbtree_search(RBTREE *tree, TYPE key)
 		if (tree->key > key)
 			tree = tree->left;
 		else
-			tree = tree->right
+			tree = tree->right;
 	}
 
 	return tree;
@@ -121,55 +121,233 @@ int rbtree_minimum(RBROOT *root, TYPE *val)
 /*
  * Rotate a tree to the left.
  * */
-static void rbtree_left_rotate(RBROOT *root, RBTREE *tree)
+static void rbtree_left_rotate(RBROOT *root, RBTREE *x)
 {
-	RBTREE *node = tree->right;
+	RBTREE *y = x->right;
 
-	tree->right = node->left;
-	if (node->left != NULL)
-		node->left->parent = tree;
+	x->right = y->left;
+	if (y->left != NULL)
+		y->left->parent = x;
 
-	node->parent = tree->parent;
+	y->parent = x->parent;
 
-	if (tree->parent == NULL)
-		root->node = node;
+	if (x->parent == NULL)
+		root->node = y;
 	else {
 		/* If tree was a left child before, then node should 
 		 * also be a left child, and vice versa. */
-		if (tree->parent->left == tree)
-			x->parent->left = node;
+		if (x->parent->left == x)
+			x->parent->left = y;
 		else
-			x->parent->right = node;
+			x->parent->right = y;
 	}
 
 	/* When complete rotate, node will become parent of tree, and
 	 * tree is node's child. */
-	node->left = tree;
-	tree->parent = node;
+	y->left = x;
+	x->parent = y;
 }
 
 /*
  * Rotate a tree to the right.
  * */
-static void rbtree_right_rotate(RBROOT *root, RBTREE *tree)
+static void rbtree_right_rotate(RBROOT *root, RBTREE *y)
 {
-	RBTREE *node = tree->left;
+	RBTREE *x = y->left;
 	
-	node->left = tree->right;
+	y->left = x->right;
 	if (x->right != NULL)
-		tree->right->parent = node;
+		x->right->parent = y;
 
-	tree->parent = node->parent;
+	x->parent = y->parent;
 
-	if (node->parent == NULL)
-		root->node = tree;
+	if (y->parent == NULL)
+		root->node = x;
 	else {
-		if (node->parent->right == node)
-			node->parent->right = tree;
+		if (y->parent->right == y)
+			y->parent->right = x;
 		else
-			node->parent->left = tree;
+			y->parent->left = x;
 	}
 
-	tree->right = node;
-	node->parent = tree;
+	x->right = y;
+	y->parent = x;
+}
+
+
+static void rbtree_insert_fixup(RBROOT *root, RBTREE *node)
+{
+	RBTREE *parent, *gparent;
+
+	while ((parent = RB_PARENT(node)) && RB_IS_RED(parent)) {
+		gparent = RB_PARENT(parent);
+
+		if (parent == gparent->left) {
+			{
+				RBTREE *uncle = gparent->right;
+				if (uncle && RB_IS_RED(uncle)) {
+					RB_SET_BLACK(uncle);
+					RB_SET_BLACK(parent);
+					RB_SET_RED(gparent);
+					node = gparent;
+					continue;
+				}
+			}
+			
+			/* case 2:
+			 * uncle is black, and node is right child. */
+			if (parent->right == node) {
+				RBTREE *tmp;
+				rbtree_left_rotate(root, parent);
+				tmp = parent;
+				parent = node;
+				node = tmp;
+			}
+
+			/* case 3:
+			 * uncle is black, and node is left child. */
+			RB_SET_BLACK(parent);
+			RB_SET_RED(gparent);
+			rbtree_right_rotate(root, gparent);
+		} else {//
+			/* uncle is red*/
+			{
+				RBTREE *uncle = gparent->left;
+				if (uncle && RB_IS_RED(uncle)) {
+					RB_SET_BLACK(uncle);
+					RB_SET_BLACK(parent);
+					RB_SET_RED(gparent);
+					node = gparent;
+					continue;
+				}
+			}
+
+			if (parent->left == node) {
+				RBTREE *tmp;
+				rbtree_right_rotate(root, parent);
+				tmp = parent;
+				parent = node;
+				node = tmp;
+			}
+
+			RB_SET_BLACK(parent);
+			RB_SET_RED(gparent);
+			rbtree_left_rotate(root, gparent);
+
+		}
+	}
+	/* root node need to be black */
+	RB_SET_BLACK(root->node);
+}
+
+static void _rbtree_insert(RBROOT *root, RBTREE *node)
+{
+	RBTREE *y = NULL;
+	RBTREE *x = root->node;
+
+	while (x != NULL) {
+		y = x;
+		if (node->key < x->key)
+			x = x->left;
+		else
+			x = x->right;
+	}
+	RB_PARENT(node) = y;
+
+	if (y != NULL) {
+		if (node->key < y->key)
+			y->left = node;
+		else
+			y->right = node;
+	} else {
+		root->node = node;
+	}
+
+	node->color = RED;
+
+	rbtree_insert_fixup(root, node);
+}
+
+static RBTREE* create_rbtree_node(TYPE key, RBTREE *parent, 
+		RBTREE *left, RBTREE *right)
+{
+	RBTREE *p;
+	if ((p = (RBTREE*)malloc(sizeof(RBTREE))) == NULL)
+		return NULL;
+
+	p->key = key;
+	p->left = left;
+	p->right = right;
+	p->parent = parent;
+	p->color = BLACK; //default color
+
+	return p;
+}
+
+
+/*
+ * Create a node, and insert it to rbtree.
+ *
+ * @root: root of red-black tree
+ * @key : key value of waiting to be insertd  
+ * @return
+ * -1, insert failure
+ * 0, insert success
+ **/
+int rbtree_insert(RBROOT *root, TYPE key)
+{
+	RBTREE *node;
+
+	if (_recursive_rbtree_search(root->node, key) != NULL)
+		return -1;
+
+	if ((node = create_rbtree_node(key, NULL, NULL, NULL)) == NULL)
+		return -1;
+
+	_rbtree_insert(root, node);
+
+	return 0;
+}
+
+void _rbtree_delete(RBROOT *root, RBTREE *node)
+{
+
+}
+
+void rbtree_delete(RBROOT *root, TYPE key)
+{
+	RBTREE *z, *node;
+
+	if ((z = _recursive_rbtree_search(root->node, key)) != NULL)
+		_rbtree_delete(root, z);
+}
+
+/*
+ * Print red-black tree. 
+ *
+ * @tree : node of red-black tree
+ * @key  : value of node
+ * @direction: 
+ * 		0, the node is root,
+ * 		-1, the node is left child,
+ * 		1, the node is right child.
+ * */
+static void _rbtree_print(RBTREE *tree, TYPE key, int direction)
+{
+	if (tree != NULL) {
+		if (direction == 0)
+			printf("%2d(B) is root\n", tree->key);
+		else
+			printf("%2d(%s) is %2d's %6s child\n", 
+					tree->key, RB_IS_RED(tree)?"R":"B",
+					key, direction==1?"right":"left");
+		_rbtree_print(tree->left, tree->key, -1);
+		_rbtree_print(tree->right, tree->key, 1);
+	}
+}
+
+void rbtree_print(RBROOT *root)
+{
+	if (root != NULL && root->node != NULL)
+		_rbtree_print(root->node, root->node->key, 0);
 }
--- C-Library/rbtree.h
+++ C-Library/rbtree.h
@@ -1,16 +1,20 @@
 #ifndef _RED_BLACK_TREE_H_
 #define _RED_BLACK_TREE_H_
 
+#include <stdint.h>
 #define RED		0   //red node
 #define BLACK	1	//black node
+typedef int TYPE;
 
-struct rbtree_node {
-	uint32_t char color;
+#define likely(x)	__builtin_expect(!!(x), 1)
+#define unlikely(x)	__builtin_expect(!!(x), 0)
+typedef struct rbtree_node {
+	unsigned char color;
 	int key;
 	struct rbtree_node *left;  	//left child
 	struct rbtree_node *right;	//right child
 	struct rbtree_node *parent; //parent
-};
+}RBTREE;
 
 typedef struct rbtree_root {
 	struct rbtree_node *node;
@@ -18,9 +22,9 @@ typedef struct rbtree_root {
 
 /* create a rbtree, and return the root of tree. */
 RBROOT* create_rbtree();
-void destroy_rbtree(RBROOT*);
-int intsert_rbtree(RBROOT*, int key);
-void delete_rbtree(RBROOT*, int key);
+void rbtree_destroy(RBROOT*);
+int rbtree_insert(RBROOT*, int key);
+void rbtree_delete(RBROOT*, int key);
 
 /**/
 void preorder_rbtree(RBROOT*);
@@ -33,6 +37,6 @@ int iterative_rbtree_search(RBROOT*, int key);
 int rbtree_minimum(RBROOT*, int *val);
 int rbtree_maximum(RBROOT*, int *val);
 
-void print_rbtree(RBROOT*);
+void rbtree_print(RBROOT*);
 
 #endif
